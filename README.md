dab0c2d570ad4fc0dc1ba591093a0c8ea0fa7ffb
Xavier Shay <contact@xaviershay.com>
2017-02-19 22:38:58 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+A WIP mod for Factorio to dump all available recipes to JSON. It currently
+outputs them to Factorio log (`factorio-current.log` in app data directory),
+though it would be nice some day to go to a separate file (not sure if
+possible).
+
+Includes a vendored copy of a JSON library I didn't write.
+
+## Usage
+
+    /c remote.call("json-recipes", "dump")
---
+++ control.lua
@@ -0,0 +1,33 @@
+local json = require "json"
+
+interface = {}
+interface.dump = function()
+	-- To ensure they are all there...
+	game.player.force.research_all_technologies()
+
+	ret = {}
+	for name, data in pairs(game.player.force.recipes) do
+		ret[name] = {
+		  ingredients = {},
+	          products    = {}
+	        }
+		for _, i in ipairs(data.ingredients) do
+			x = {}
+			x['type'] = i.type
+			x['name'] = i.name
+			x['amount'] = i.amount
+			table.insert(ret[name]["ingredients"], x)
+		end
+		for _, i in ipairs(data.products) do
+			y = {}
+			y['type'] = i.type
+			y['name'] = i.name
+			y['amount'] = i.amount
+			-- TODO: Optionally include amount_min/max/probability
+			table.insert(ret[name]["products"], y)
+		end
+	end
+	log(json:encode(ret))
+	game.player.print("All recipes dumped to log file as JSON")
+end
+remote.add_interface("json-recipes", interface)
